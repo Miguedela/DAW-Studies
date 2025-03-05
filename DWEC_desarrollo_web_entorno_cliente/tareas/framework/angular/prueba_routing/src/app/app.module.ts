bbc528bfa6c5d4b5f6978e9e44130651d9608c17
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-03-05 17:34:35 +0100

    add Home, ListadoProductos, ListadoClientes, and Pagina404 components with routing setup

--- DWEC_desarrollo_web_entorno_cliente/tareas/framework/angular/prueba_routing/src/app/app.module.ts
+++ DWEC_desarrollo_web_entorno_cliente/tareas/framework/angular/prueba_routing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListadoProductosComponent,
+    ListadoClientesComponent,
+    Pagina404Component
   ],
   imports: [
     BrowserModule,
---
+++ DWEC_desarrollo_web_entorno_cliente/tareas/framework/angular/prueba_routing/src/app/listado-clientes/listado-clientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-clientes',
+  standalone: false,
+  templateUrl: './listado-clientes.component.html',
+  styleUrl: './listado-clientes.component.css'
+})
+export class ListadoClientesComponent {
+
+}
