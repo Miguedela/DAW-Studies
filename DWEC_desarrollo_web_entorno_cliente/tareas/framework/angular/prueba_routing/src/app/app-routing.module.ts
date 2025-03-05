3793e0e8c574adda44151e0fb1819007c9e36d9c
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-03-05 17:41:44 +0100

    add routing configuration and update index.html to include Bootstrap CSS

--- DWEC_desarrollo_web_entorno_cliente/tareas/framework/angular/prueba_routing/src/app/app-routing.module.ts
+++ DWEC_desarrollo_web_entorno_cliente/tareas/framework/angular/prueba_routing/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'listadoclientes', component: ListadoClientesComponent },
+  { path: 'listadoproductos', component: ListadoProductosComponent },
+  { path: '**', component: Pagina404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
